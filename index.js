a127cbbdc47791c1a164b3e16c70afc0fff23ab8
sboamah <60356081+sboamah@users.noreply.github.com>
2021-03-28 17:53:55 -0400

    Complete HW4

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ if (process.env.NODE_ENV != 'test')
 {
 	(async () => {
 		await listAuthenicatedUserRepos();
-		//await listBranches(userId, "your repo");
-		//await createRepo(userId,newrepo);
-		//await createIssue(userId, repo, issue);
-		//await enableWikiSupport(userId,repo);
+		await listBranches(userId, "your repo");
+		await createRepo(userId,newrepo);
+		await createIssue(userId, repo, issue);
+		await enableWikiSupport(userId,repo);
 
 	})()
 }
@@ -94,19 +94,29 @@ function listAuthenicatedUserRepos()
 // 1. Write code for listBranches in a given repo under an owner. See list branches
 async function listBranches(owner,repo)
 {
-	let options = getDefaultOptions(`/`, "GET");
-	await octokit.request('GET /repos/{owner}/{repo}/branches', {
-        owner: 'owner',
-        repo: 'repo'
-      });
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/branches`, "GET");
+
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
-		request(options, function (error, response, body) {
+		request(options, function (error, response, body) 
+		{
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
 
-			// console.debug( options );
-			resolve( JSON.parse(body) );
+			var obj = JSON.parse(body);
+			for( var i = 0; i < obj.length; i++ )
+			{
+				var name = obj[i].name;
+				console.log( name );
+			}
 
+			// Return object for people calling our method.
+			resolve( obj );
 		});
 	});
 
@@ -115,34 +125,51 @@ async function listBranches(owner,repo)
 // 2. Write code to create a new repo
 async function createRepo(owner,repo)
 {
-	let options = getDefaultOptions("/", "POST");
-	await octokit.request('POST /user/repos', {
-        name: 'name'
-    });
+	let options = getDefaultOptions("/user/repos", "POST");
+	options.body = JSON.stringify( {
+		name: repo,
+		description: 'test-HW4-345'
+	});
+
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
+			
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
 
 			resolve( response.statusCode );
 
 		});
 	});
 
+
 }
 // 3. Write code for creating an issue for an existing repo.
 async function createIssue(owner,repo, issueName, issueBody)
 {
-	let options = getDefaultOptions("/", "POST");
-	await octokit.request('POST /repos/{owner}/{repo}/issues', {
-        owner: 'owner',
-        repo: 'repo',
-        title: issueName
-      });
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/issues`, "POST");
+	options.body = JSON.stringify( {
+		title: issueName,
+		body: issueBody
+	});
+
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
+
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
 
 			resolve( response.statusCode );
 
@@ -153,16 +180,22 @@ async function createIssue(owner,repo, issueName, issueBody)
 // 4. Write code for editing a repo to enable wiki support.
 async function enableWikiSupport(owner,repo)
 {
-	let options = getDefaultOptions("/", "PATCH");
-	await octokit.request('PATCH /repos/{owner}/{repo}', {
-        owner: 'owner',
-        repo: 'repo',
-        name: 'name'
-      });
+	let options = getDefaultOptions(`/repos/${owner}/${repo}`, "PATCH");
+	options.body = JSON.stringify( {
+		has_wiki: true
+	});
+
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
+
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
 
 			resolve( JSON.parse(body) );
 		});
